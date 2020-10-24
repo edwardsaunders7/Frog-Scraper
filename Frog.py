6dab0c70cf4c7cb127b27c478067e9f0135f1376
edwardsaunders7 <51308267+edwardsaunders7@users.noreply.github.com>
2020-10-24 23:25:15 +0100

    Added comments to code

--- Frog.py
+++ Frog.py
@@ -4,18 +4,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 chromeOptions = Options()
-chromeOptions.headless = True
+chromeOptions.headless = True #set headless so page does not pop up and disturb user
 
-count = int(input("How many frog pictures would you like? "));
-count2 = count
+count = int(input("How many frog pictures would you like? ")); #Ask the user how many pictures they want
+count2 = count #set this only for last line
 
-while count > 0:
-    PATH = "driver/chromedriver"
-    driver = webdriver.Chrome(PATH, options = chromeOptions)
-    driver.get("http://allaboutfrogs.org/funstuff/randomfrog.html")
-    search = driver.find_element_by_css_selector('body > font > table > tbody > tr > td > img')
-    search.screenshot(f"Pics/Frog{count}.png")
-    count = count - 1
+while count > 0: #set up the while loop to count down from user's request
+    PATH = "driver/chromedriver" # location of the chrome driver
+    driver = webdriver.Chrome(PATH, options = chromeOptions) #applying headless setting above
+    driver.get("http://allaboutfrogs.org/funstuff/randomfrog.html") #access the random frog generating page
+    search = driver.find_element_by_css_selector('body > font > table > tbody > tr > td > img') #ind the image by css selector
+    search.screenshot(f"Pics/Frog{count}.png") #save the image with appropiate filename
+    count = count - 1 #count down by one to iterate to next picture
 
-print(f"Done! Collected {count2} frog pics!")
-driver.quit()
+driver.quit() #close the chrome driver
+print(f"Done! Collected {count2} frog pics!") #Print that all frogs have been collected
